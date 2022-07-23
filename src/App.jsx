57e7d2672b07666df8649e09bd78c3bc5709afc6
Minh Vy Ha <vyha0512@gmail.com>
2022-07-23 10:22:01 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import MainContent from './MainContent'
 
 export default function App(){
   return (
-    <>
+    <div className='main-container'>
       <MainContent />
-    </>
+    </div>
   )
 }
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -13,17 +13,23 @@ export default function MainContent(){
                     <h4 className='role'>Full-Stack Developer</h4>
                     <h5 className='web'>minhvyha.tech</h5>
                     <div className='connect'>
-                        <a href="mailto:MinhVy.Ha@outlook.com">Email</a>
-                        <a href="https://www.linkedin.com/in/minhvyha/" target='_blank'>LinkedIn</a>
+                        <a className='mail' href="mailto:MinhVy.Ha@outlook.com"><img src="src/assets/Mail.svg" alt="" /> Email</a>
+                        <a className='linkedin' href="https://www.linkedin.com/in/minhvyha/" target='_blank'><img src="src/assets/linkedin.svg" alt="" />LinkedIn</a>
                     </div>
                     <div className='about'>
                         <h3>About</h3>
-                        <p>I am an aspiring full-stack developer. I am constantly learning new technologies and knowledge related to my future career. I am excited about all the opportunities where I can work and learn new technologies related to my career. I love connecting with new people, give me a shout at Minhvy.ha@outlook.com.</p>
+                        <p>I am an aspiring full-stack developer. I am constantly learning new technologies and knowledge related to my future career. </p>
                     </div>
                     <div className='interest'>
                         <h3>Interests</h3>
-                        <p>Finance. Music. Technology. Sport. Health.</p>
+                        <p>Finance. Music. Technology. Sport. Health. I love connecting with new people, give me a shout at Minhvy.ha@outlook.com.</p>
                     </div>
+                </div>
+                <div className='footer'>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href="https://www.youtube.com/channel/UC6kYs1bW0j273GneHxFyPFQ"><img className='youtube' src="src/assets/youtube.svg" alt="youtube" /></a>
+                    <a href="https://github.com/minhvy828"><img src="src/assets/GitHub Icon.svg" alt="github" /></a>
                 </div>
             </div>
         </>
